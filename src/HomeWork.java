709975da65447e25b05fcf01cec7ba47682c231c
Pavel <khilov.pavel@eyandex.ru>
2023-05-20 23:02:23 +0300

    OOP_HomeWork4

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -19,6 +19,33 @@ public class HomeWork {
     public static void main(String[] args) {
 
         Box<Apple> apples = new Box<>();
+
+        Apple apple1 = new Apple();
+        apples.addFruit(apple1);
+
+        System.out.println(apples.weightBox());
+
+        Box<Orange> oranges = new Box<>();
+
+        Orange orange1 = new Orange();
+        oranges.addFruit(orange1);
+
+        System.out.println(oranges.weightBox());
+
+        System.out.println(apples.compareBox(oranges));
+
+        System.out.println(apples.getClass());
+
+//        Box<Apple> apples2 = new Box<>();
+//        apples2.addFruit(new Apple());
+//        System.out.println(apples2.weightBox());
+//
+//        apples2.replaceBox(oranges);
+//        apples2.replaceBox(apples);
+//
+//        System.out.println(apples.weightBox());
+//        System.out.println(oranges.weightBox());
+//        System.out.println(apples2.weightBox());
     }
 }
 
@@ -32,6 +59,11 @@ abstract class Fruit{
     public Fruit(float weight) {
         this.weight = weight;
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
 
 class Apple extends Fruit{
@@ -39,8 +71,57 @@ class Apple extends Fruit{
     public Apple() {
         super(1.0f);
     }
+
+    @Override
+    public String toString() {
+        return "Apple";
+    }
 }
 
-class Box<T>{
-    ArrayList<T> fruits;
+class Orange extends Fruit{
+
+    public Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString() {
+        return "Orange";
+    }
+}
+
+class Box<T extends Fruit>{
+    private ArrayList<T> fruits = new ArrayList<>();
+
+    public void addFruit(T fruit){
+        fruits.add(fruit);
+    }
+
+    public float weightBox(){
+        float weightBox = 0;
+        for (T fruit: fruits) {
+            weightBox += fruit.getWeight();
+        }
+        return weightBox;
+    }
+
+    public boolean compareBox(Box box2){
+        return weightBox() == box2.weightBox();
+    }
+
+//    public void replaceBox(Box box2){
+//        if (fruits.getClass() == box2.getClass()){
+//            for (T fruit: fruits) {
+//                box2.addFruit(fruit);
+//                fruits.remove(fruit);
+//            }
+//        } else{
+//            System.out.println("В коробках лежат разные фрукты.");
+//        }
+//    }
+
+    @Override
+    public String toString() {
+        return String.format("В коробке лежат фрукты общим весом %.2f", weightBox());
+    }
 }
